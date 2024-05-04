16c46aee33597bacab1ef84fe36fe4422ac9d576
Manoj Sai Charan Mangena <mmangena@nextsphere.com>
2024-05-04 17:13:20 +0530

    Removed all unwnated code

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TASK=`sudo aws ecs list-tasks --cluster DEMO --service-name Demo_app --desired-status RUNNING --region ap-south-1 --output text | awk '{print $2}'`
-sudo aws ecs stop-task --cluster DEMO --task $TASK
-echo "Task stopped successfully"
