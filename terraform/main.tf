d20edcf13c5051221311725ae2ef78aa130ee223
Manoj Mangena <70632603+manoj121999@users.noreply.github.com>
2024-05-05 16:45:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -194,19 +194,26 @@ resource "aws_instance" "ci-capstone-jenkins" {
     private_key = file("${var.private_key_path}")
     host        = self.public_ip
   }
-
+ provisioner "file" {
+    source      = "jenkins_config.tar"
+    destination = "/home/ubuntu/jenkins_latest.tar"
+  }
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc",
       "echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt update",
-      "sudo apt install jenkins openjdk-17-jdk docker.io -y",
+      "sudo apt install jenkins openjdk-17-jdk docker.io awscli -y",
       "echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers",
       "sudo systemctl start jenkins",
       "suoo docker network create sonar-network",
       "sudo docker run -d --name sonar-db --network sonar-network -e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonar -e POSTGRES_DB=sonar postgres:9.6",
-      "sudo docker run -d --name sonar -p 9000:9000 --network sonar-network -e SONARQUBE_JDBC_URL=jdbc:postgresql://sonar-db:5432/sonar -e SONAR_JDBC_USERNAME=sonar -e SONAR_JDBC_PASSWORD=sonar sonarqube"
+      "sudo docker run -d --name sonar -p 9000:9000 --network sonar-network -e SONARQUBE_JDBC_URL=jdbc:postgresql://sonar-db:5432/sonar -e SONAR_JDBC_USERNAME=sonar -e SONAR_JDBC_PASSWORD=sonar sonarqube",
+      "sudo rm -r /var/lib/jenkins/plugins /var/lib/jenkins/jobs /var/lib/jenkins/credentials.xml",
+      "sudo tar -xvzf /home/ubuntu/jenkins_latest.tar -C /var/lib/jenkins",
+      "sudo chown -R jenkins:jenkins /var/lib/jenkins/",
+      "sudo systemctl restart jenkins"
 
     ]
   }
